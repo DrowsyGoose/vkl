0afb743b256f6f09fc33e318ce316a40bde8e56a
DrowsyGoose <74874445+DrowsyGoose@users.noreply.github.com>
2020-11-22 21:25:35 +0200

    Update first.py

--- first.py
+++ first.py
@@ -2,13 +2,21 @@ import discord
 from discord.ext import commands
 import os 
 import random
+import asyncio
 
 bot = commands.Bot(command_prefix='>')
 
-@bot.command()
+@bot.event
+async def on_ready():
+    print("Раб готов к работе")
+    
+@bot.command(pass_context = True)
 async def mydick(ctx):
-    piska=random.randint(1,41)
-    await ctx.send('Бот измерял твой писюн. Результат:', piska ,'см')
+    emded = discord.Emded(title = "Бот измерял твою пипиську. Результат:", description = (random.randint(1,41)), color = 
+(0xF85252))
+    await ctx.send(emded = emded)
+
+
 
 token = os.environ.get('BOT_TOKEN')
 
