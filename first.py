f8d97ae0da73132c1359801cd176d924bb17b9c7
DrowsyGoose <74874445+DrowsyGoose@users.noreply.github.com>
2020-12-06 18:34:18 +0200

    Update first.py

--- first.py
+++ first.py
@@ -155,23 +155,23 @@ async def on_message(message):
 async def update_data(users, user):
     if not f'{user.id}' in users:
         users[f'{user.id}'] = {}
-        users[f'{user.id}']['experience'] = 0
-        users[f'{user.id}']['level'] = 1
+        users[f'{user.id}']['опыт'] = 0
+        users[f'{user.id}']['уровень'] = 1
 
 
 async def add_experience(users, user, exp):
-    users[f'{user.id}']['experience'] += exp
+    users[f'{user.id}']['опыт'] += exp
 
 
 async def level_up(users, user, message):
     with open('levels.json', 'r') as g:
         levels = json.load(g)
-    experience = users[f'{user.id}']['experience']
-    lvl_start = users[f'{user.id}']['level']
+    experience = users[f'{user.id}']['опыт']
+    lvl_start = users[f'{user.id}']['уровень']
     lvl_end = int(experience ** (1 / 4))
     if lvl_start < lvl_end:
-        await message.channel.send(f'{user.mention} has leveled up to level {lvl_end}')
-        users[f'{user.id}']['level'] = lvl_end
+        await message.channel.send(f'{user.mention} поднял уровень до {lvl_end}')
+        users[f'{user.id}']['уровень'] = lvl_end
 
 @bot.command()
 async def level(ctx, member: discord.Member = None):
@@ -179,14 +179,14 @@ async def level(ctx, member: discord.Member = None):
         id = ctx.message.author.id
         with open('users.json', 'r') as f:
             users = json.load(f)
-        lvl = users[str(id)]['level']
-        await ctx.send(f'You are at level {lvl}!')
+        lvl = users[str(id)]['уровень']
+        await ctx.send(f'У тебя {lvl} уровень!')
     else:
         id = member.id
         with open('users.json', 'r') as f:
             users = json.load(f)
-        lvl = users[str(id)]['level']
-        await ctx.send(f'{member} is at level {lvl}!')
+        lvl = users[str(id)]['уровень']
+        await ctx.send(f'{member} имеет {lvl} уровень!')
         
 
 
