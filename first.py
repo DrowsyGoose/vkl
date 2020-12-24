1438d80cb6be04885f1be3ac9c8568996d874ad0
DrowsyGoose <74874445+DrowsyGoose@users.noreply.github.com>
2020-12-24 19:56:45 +0200

    Update first.py

--- first.py
+++ first.py
@@ -19,7 +19,7 @@ bot = commands.Bot(command_prefix='>')
 @bot.event
 async def on_member_join(member):
      role = discord.utils.get(member.server.roles, name="unverified") 
-     await client.add_roles(member, role)
+     await bot.add_roles(member, role)
 @bot.command()
 async def server(ctx):
     serv = MinecraftServer.lookup("mc.reworlds.ru")
