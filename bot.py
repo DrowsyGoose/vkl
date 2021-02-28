43a5f3cb4b8f5eca30a4cade8e4ac9d9c527ed63
DrowsyGoose <74874445+DrowsyGoose@users.noreply.github.com>
2021-02-28 16:45:02 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,8 +4,8 @@ from discord.ext import commands
 bot = commands.Bot(command_prefix='-')
 
 @bot.command()
-async def test(ctx):
-    await ctx.send("work")
+async def ping(ctx):
+    await ctx.send("pong")
     
 token = os.environ.get("BOT_TOKEN")
     
