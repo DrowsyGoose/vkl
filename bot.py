795057ed948d7702ac890c34e7a3c27ca1036b6e
DrowsyGoose <74874445+DrowsyGoose@users.noreply.github.com>
2021-03-11 10:19:30 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,10 +16,13 @@ async def ping(ctx):
     await ctx.send("pong")
     
 @bot.command()
-async def who(ctx):
+async def who(ctx, user: discord.Member):
     pros = random.randint(0, 100)
     ari = random.choice(["Автосексуал", "Андрогинсексуал", "Андросексуал", "Асексуал", "Бисексуал", "Гетеросексуал", "Гиносексуал", "Гомосексуал", "Грейсексуал", "Демисексуал", "Литосексуал", "Объектумсексуал", "Омнисексуал", "Пансексуал", "Полисексуал", "ПоМосексуал", "Сапиосексуал", "Сколиосексуал", "Цифросексуал"])
-    embed=discord.Embed(title="**Геемер**", description= f"Вы {ari} на {pros}%", color=0xff0000)
+    if user = ctx.author:
+        embed=discord.Embed(title="**Геемер**", description= f"Вы {ari} на {pros}%", color=0xff0000)
+    else:
+        embed=discord.Embed(title="**Геемер**", description= f"{user} {ari} на {pros}%", color=0xff0000)
     await ctx.send(embed=embed)
     #d
                                     
