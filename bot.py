97b6ec815c842618e7aafd8a1218dd618476c5c3
DrowsyGoose <74874445+DrowsyGoose@users.noreply.github.com>
2021-07-06 20:44:04 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,10 +2,7 @@ import discord
 from discord.ext import commands
 import os
 import random
-
-from discord.ext.commands import Bot
-
-from discord_components import DiscordComponents, Button, Select, SelectOption
+import discord_components 
 
 
 
